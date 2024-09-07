e74aff3855726de0284a0a16d57a55a65380c443
ratqe <ivanvay86@gmail.com>
2024-09-07 19:14:18 +1200

    Pause Menu
    In-game pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true); // Activate the menu
+        Time.timeScale = 0f; // Stop the in-game clock
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false); // Deactivate the menu
+        Time.timeScale = 1f; // Resume the in-game clock
+        isPaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
