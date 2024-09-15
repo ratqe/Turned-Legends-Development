c61f328b707e4df8343c5f64e1589c5040d28a17
vexmomo <taransingh4644@gmail.com>
2024-09-15 22:57:45 +1200

    item shop v1
    baseplate for the item this would be changed as we move forward

---
+++ Assets/Scripts/ShopManagerScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ShopManagerScript : MonoBehaviour
+{
+
+    public int [,] shopItems = new int [5,5];
+    public float coins;
+    public Text CoinsAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoinsAmount.text = "Coins:" + coins.ToString();
+
+        // items
+        shopItems [1,1] = 1;
+        shopItems [1,2] = 2;
+        shopItems [1,3] = 3;
+        shopItems [1,4] = 4;
+
+        // prices
+        shopItems [2,1] = 10;
+        shopItems [2,2] = 50;
+        shopItems [2,3] = 100;
+        shopItems [2,4] = 250;
+        
+        // quantity
+        shopItems [3,1] = 0;
+        shopItems [3,2] = 0;
+        shopItems [3,3] = 0;
+        shopItems [3,4] = 0;
+
+    }
+
+    public void Buy()
+    {
+        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+
+        if(coins >= shopItems [2, ButtonRef.GetComponent<ButtonInfo>().ItemID])
+        {
+            coins -= shopItems [2, ButtonRef.GetComponent<ButtonInfo>().ItemID];
+            shopItems [3, ButtonRef.GetComponent<ButtonInfo>().ItemID]++;
+            CoinsAmount.text = "Coins:" + coins.ToString();
+            ButtonRef.GetComponent<ButtonInfo>().Quantity.text = shopItems [3, ButtonRef.GetComponent<ButtonInfo>().ItemID].ToString();
+        }
+    }
+}
