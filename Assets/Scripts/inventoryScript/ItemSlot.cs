7c631bd8a4f38dec1f575a3225850880934c6afa
vexmomo <taransingh4644@gmail.com>
2024-09-19 23:37:00 +1200

    inventory v2.2
    when the user clicks on a potion slot it highlights its self to show its chosen

--- Assets/Scripts/inventoryScript/ItemSlot.cs
+++ Assets/Scripts/inventoryScript/ItemSlot.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Unity.VisualScripting;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     // Item Data
     public string itemName;
@@ -19,6 +21,16 @@ public class ItemSlot : MonoBehaviour
     [SerializeField]
     private Image itemImage;
 
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+
+    private InventoryManager inventoryManager;
+
+    private void Start()
+    {
+        inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+    }
+
     public void AddItem(string itemName, int quantity, Sprite itemSprite)
     {
         this.itemName = itemName;
@@ -29,5 +41,30 @@ public class ItemSlot : MonoBehaviour
         quantityText.text = quantity.ToString();
         quantityText.enabled = true;
         itemImage.sprite = itemSprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left)
+        {
+            OnLeftClick();
+        }
+
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightClick();
+        }
+    }
+
+    public void OnLeftClick()
+    {
+        inventoryManager.DeselectAllSlots();
+        selectedShader.SetActive(true);
+        thisItemSelected = true;
+    }
+
+    public void OnRightClick()
+    {
+
     }
 }
