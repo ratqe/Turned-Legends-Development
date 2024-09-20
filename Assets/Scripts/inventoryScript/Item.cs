f7bfc91d66a2222ff3cf33068ea9f885fb9522c6
vexmomo <taransingh4644@gmail.com>
2024-09-20 15:03:51 +1200

    inventory v2.3
    when the items are picked up they go into the users inventory on when the user clicks on a potion it will show the name and description of the potion

--- Assets/Scripts/inventoryScript/Item.cs
+++ Assets/Scripts/inventoryScript/Item.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
 
-    [SerializeField] private string itemName;
-    [SerializeField] private int quantity;
-    [SerializeField] private Sprite sprite;
+    [SerializeField]
+    private string itemName;
+    
+    [SerializeField]
+    private int quantity;
+    
+    [SerializeField]
+    private Sprite sprite;
+
+    [TextArea]
+    [SerializeField]
+    private string itemDescription;
+
     private InventoryManager inventoryManager;
     // Start is called before the first frame update
     void Start()
@@ -19,7 +29,7 @@ public class Item : MonoBehaviour
 {
     if (collision.gameObject.tag == "Player")
     {
-        inventoryManager.AddItem(itemName, quantity, sprite);
+        inventoryManager.AddItem(itemName, quantity, sprite, itemDescription);
         Destroy(gameObject);
     }
 }
